ca3d5841420a48568e29c28604746db472696b02
Dzamora89 <92052822+Dzamora89@users.noreply.github.com>
2023-02-03 00:13:02 +0100

    Race Admin con el cambio en la SQL Necesario

--- AdminSide/Race/RaceAdmin.js
+++ AdminSide/Race/RaceAdmin.js
@@ -8,19 +8,18 @@ function fillTheTable() {
 
     fetch("http://localhost/gt3prostats/api/race/getallRace.php", requestOptions)
         .then(response => response.json())
-        .then(data => data.forEach((dato) => {
-            console.log(dato)
-            $('#raceTable').append(`<tr>
+        .then(dataRaces => {
+            dataRaces.forEach((dato) =>{
+                $('#raceTable').append(`<tr>
                 <td>${dato.raceID}</td>
                 <td>${dato.track}</td>
                 <td>${dato.dateOfRace}</td>
                 <td>${dato.country}</td>
-                <td>${dato.championshipID}</td>
+                <td >${dato.name}</td>
                 </tr>`)
-
-
-        }))
+            })})
         .catch(error => console.log('error', error));
 
 
 }
+
